4509c9a13554da7e9254251ea68154a8e788e4e7
mfbieber <mfbieber@gmail.com>
2019-01-11 20:21:59 +0100

    Started adding some tests for TDD for Day02, part 1.

--- src/main/java/org/basseur/adventofcode/advent2018/Days/Day02.java
+++ src/main/java/org/basseur/adventofcode/advent2018/Days/Day02.java
@@ -2,10 +2,16 @@ package org.basseur.adventofcode.advent2018.Days;
 
 public class Day02 extends Days {
 
+    String testBoxIDs;
+
     public String firstPart(){
-        return "Part 1: ";
+        return "Part 1 - Checksum: " + calculateChecksum(testBoxIDs);
     }
     public String secondPart(){
         return "Part 2: ";
+    }
+
+    public int calculateChecksum(String testBoxIDs) {
+        return 0;
     }
 }
