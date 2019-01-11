ed83b29fc284fb2faef668d53b7e38ec762a33c5
mfbieber <mfbieber@gmail.com>
2019-01-11 18:58:12 +0100

    Added Unit Tests fpr Day1, part 1

--- src/test/java/org/basseur/adventofcode/advent2018/Days/Day01Test.java
+++ src/test/java/org/basseur/adventofcode/advent2018/Days/Day01Test.java
@@ -1,24 +1,96 @@
 package org.basseur.adventofcode.advent2018.Days;
 
-import org.junit.Before;
+import org.basseur.adventofcode.advent2018.Utils.FileReaders;
+import org.junit.Assert;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
-import java.io.FileNotFoundException;
-
-@RunWith(SpringRunner.class)
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ FileReaders.class })
 public class Day01Test {
 
     Day01 subjectToTest = new Day01();
+    Integer[] frequencies;
 
-    @Before
-    public void setup() {
+    @Test
+    public void calculateFrequencyAddsFrequenciesCorrectlyForFirstExample() {
+        //arrange
+        frequencies = new Integer[4];
+        frequencies[0] = 1;
+        frequencies[1] = -2;
+        frequencies[2] = 3;
+        frequencies[3] = 1;
+        PowerMockito.mockStatic(FileReaders.class);
+        PowerMockito.when(FileReaders.readFileIntoArrayOfIntegers(Mockito.anyString())).thenReturn(frequencies);
+
+        String expectedResult = "Part 1 - Frequency: " + 3;
+
+        //act
+        String actualResult = subjectToTest.firstPart();
+
+        //assert
+        Assert.assertEquals(expectedResult, actualResult);
     }
 
-    public void test() {
+    @Test
+    public void calculateFrequencyAddsFrequenciesCorrectlyForSecondExample() {
         //arrange
+        frequencies = new Integer[3];
+        frequencies[0] = 1;
+        frequencies[1] = 1;
+        frequencies[2] = 1;
+        PowerMockito.mockStatic(FileReaders.class);
+        PowerMockito.when(FileReaders.readFileIntoArrayOfIntegers(Mockito.anyString())).thenReturn(frequencies);
+
+        String expectedResult = "Part 1 - Frequency: " + 3;
+
         //act
+        String actualResult = subjectToTest.firstPart();
+
         //assert
+        Assert.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void calculateFrequencyAddsFrequenciesCorrectlyForThirdxample() {
+        //arrange
+        frequencies = new Integer[3];
+        frequencies[0] = 1;
+        frequencies[1] = 1;
+        frequencies[2] = -2;
+        PowerMockito.mockStatic(FileReaders.class);
+        PowerMockito.when(FileReaders.readFileIntoArrayOfIntegers(Mockito.anyString())).thenReturn(frequencies);
+
+        String expectedResult = "Part 1 - Frequency: " + 0;
+
+        //act
+        String actualResult = subjectToTest.firstPart();
+
+        //assert
+        Assert.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void calculateFrequencyAddsFrequenciesCorrectlyForFourthxample() {
+        //arrange
+        frequencies = new Integer[3];
+        frequencies[0] = -1;
+        frequencies[1] = -2;
+        frequencies[2] = -3;
+        PowerMockito.mockStatic(FileReaders.class);
+        PowerMockito.when(FileReaders.readFileIntoArrayOfIntegers(Mockito.anyString())).thenReturn(frequencies);
+
+        String expectedResult = "Part 1 - Frequency: " + -6;
+
+        //act
+        String actualResult = subjectToTest.firstPart();
+
+        //assert
+        Assert.assertEquals(expectedResult, actualResult);
     }
 }
--- src/test/java/org/basseur/adventofcode/advent2018/Utils/FileReadersTest.java
+++ src/test/java/org/basseur/adventofcode/advent2018/Utils/FileReadersTest.java
@@ -1,5 +1,6 @@
 package org.basseur.adventofcode.advent2018.Utils;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -7,8 +8,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 public class FileReadersTest {
 
+    //TODO: not easy to test, but we should!
+    @Test
     public void readFileIntoArrayOfIntegersThrowsExceptionIfFileIsNotFound() {
-
     }
 
 }
