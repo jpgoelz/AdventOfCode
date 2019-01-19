b1b2120c392f88d51ba9d8d49cea2490f90b403a
Jan Philipp Gölz <basseur@mail.de>
2019-01-19 21:03:17 +0100

    AOC-011: Updated test for Day03 Part1

--- src/test/java/org/basseur/adventofcode/advent2018/Days/Day03Test.java
+++ src/test/java/org/basseur/adventofcode/advent2018/Days/Day03Test.java
@@ -16,23 +16,23 @@ import java.util.List;
 @PrepareForTest({FileReaders.class})
 public class Day03Test {
 
-    private Day03 subjectToTest = new Day03();
+    private List<String> claims = new ArrayList<>();
 
     @Test
     public void calculateAreaOfFabricCorrectly() {
         //arrange
-        List<String> claims = new ArrayList<>();
         claims.add("#1 @ 1,3: 4x4");
         claims.add("#2 @ 3,1: 4x4");
         claims.add("#3 @ 5,5: 2x2");
 
         PowerMockito.mockStatic(FileReaders.class);
         PowerMockito.when(FileReaders.readFileIntoStringList(Mockito.anyString())).thenReturn(claims);
+        Day03 day03 = new Day03();
 
         String expectedResult = "Part 1 - Fabric claim overlap: " + 4 + " square inches";
 
         //act
-        String actualResult = subjectToTest.firstPart();
+        String actualResult = day03.firstPart();
 
         //assert
         Assert.assertEquals(expectedResult, actualResult);
