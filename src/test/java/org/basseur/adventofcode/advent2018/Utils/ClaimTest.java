eb2e67c953d0fda7884bfdca29278fb88af56174
Jan Philipp Gölz <basseur@mail.de>
2019-01-20 03:39:43 +0100

    AOC-011: Created empty test for Claim

---
+++ src/test/java/org/basseur/adventofcode/advent2018/Utils/ClaimTest.java
@@ -0,0 +1,20 @@
+package org.basseur.adventofcode.advent2018.Utils;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ClaimTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getNumberOfMultipleClaims() {
+    }
+}
