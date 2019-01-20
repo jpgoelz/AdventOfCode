7c20ae6d55104c403b45ce7a37defd4dc1b860e1
Jan Philipp Gölz <basseur@mail.de>
2019-01-20 03:55:59 +0100

    AOC-011: Test for Day03 Part1, which does not work

--- src/test/java/org/basseur/adventofcode/advent2018/Days/Day03Test.java
+++ src/test/java/org/basseur/adventofcode/advent2018/Days/Day03Test.java
@@ -1,5 +1,6 @@
 package org.basseur.adventofcode.advent2018.Days;
 
+import org.basseur.adventofcode.advent2018.Utils.Claim;
 import org.basseur.adventofcode.advent2018.Utils.FileReaders;
 import org.junit.Assert;
 import org.junit.Test;
@@ -13,20 +14,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({FileReaders.class})
+@PrepareForTest({FileReaders.class, Claim.class})
 public class Day03Test {
 
-    private List<String> claims = new ArrayList<>();
+    private List<String> claimsList = new ArrayList<>();
 
     @Test
-    public void calculateAreaOfFabricCorrectly() {
+    public void testCalculateAreaOfFabric() {
         //arrange
-        claims.add("#1 @ 1,3: 4x4");
-        claims.add("#2 @ 3,1: 4x4");
-        claims.add("#3 @ 5,5: 2x2");
+        claimsList.add("#1 @ 1,3: 4x4");
+        claimsList.add("#2 @ 3,1: 4x4");
+        claimsList.add("#3 @ 5,5: 2x2");
+
+        int claimsListSize = claimsList.size();
+        Claim[] claims = new Claim[claimsListSize];
+        for (int i = 0; i < claimsListSize; i++) {
+            claims[i] = new Claim(claimsList.get(i));
+        }
 
         PowerMockito.mockStatic(FileReaders.class);
-        PowerMockito.when(FileReaders.readFileIntoStringList(Mockito.anyString())).thenReturn(claims);
+        PowerMockito.when(FileReaders.readFileIntoStringList(Mockito.anyString())).thenReturn(claimsList);
+
+//        PowerMockito.mockStatic(Claim.class);
+//        PowerMockito.when(Claim.getNumberOfMultipleClaims()).thenReturn(4);
+
         Day03 day03 = new Day03();
 
         String expectedResult = "Part 1 - Fabric claim overlap: " + 4 + " square inches";
