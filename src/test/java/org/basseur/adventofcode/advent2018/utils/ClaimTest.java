54064f3edb1f0f824bb3f8160446c4338ab4e394
Jan Philipp Gölz <basseur@mail.de>
2019-01-22 00:48:06 +0100

    AOC-011: Test for Claim constructor and `Claim.overlaps()`

--- src/test/java/org/basseur/adventofcode/advent2018/utils/ClaimTest.java
+++ src/test/java/org/basseur/adventofcode/advent2018/utils/ClaimTest.java
@@ -1,20 +1,52 @@
 package org.basseur.adventofcode.advent2018.utils;
 
-import org.junit.After;
-import org.junit.Before;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class ClaimTest {
 
-    @Before
-    public void setUp() {
-    }
-
-    @After
-    public void tearDown() {
-    }
-
     @Test
-    public void getNumberOfMultipleClaims() {
+    public void Claim() {
+        Claim claim = new Claim("#1 @ 2,3: 4x5");
+        int[] expectedResult = new int[]{1, 2, 3, 4, 5};
+        int[] actualResult = new int[]{claim.getId(), claim.getX1(), claim.getY1(), claim.getWidth(), claim.getHeight()};
+
+        Assert.assertArrayEquals(expectedResult, actualResult);
     }
+
+    /**
+     * claim1 is on top of claim2, no overlap.
+     * claim3 is on the right of claim1, no overlap.
+     * claim4 overlaps claim1 on the lower right corner.
+     * claim4 overlaps claim2 on the upper right corner.
+     * claim5 completely surrounds claim1 (overlaps).
+     * Gute Ideen zum testen: https://www.baeldung.com/java-check-if-two-rectangles-overlap
+     */
+    @Test
+    public void overlaps() {
+        Claim claim1 = new Claim("#1 @ 2,2: 4x3");
+        Claim claim2 = new Claim("#2 @ 2,6: 4x3");
+        Claim claim3 = new Claim("#3 @ 7,2: 4x3");
+        Claim claim4 = new Claim("#4 @ 4,4: 4x3");
+        Claim claim5 = new Claim("#5 @ 1,1: 6x5");
+
+        boolean result1 = claim1.overlaps(claim2);
+        boolean result2 = claim1.overlaps(claim3);
+        boolean result3 = claim1.overlaps(claim4);
+        boolean result4 = claim2.overlaps(claim4);
+        boolean result5 = claim1.overlaps(claim5);
+
+        Assert.assertFalse(result1);
+        Assert.assertFalse(result2);
+        Assert.assertTrue(result3);
+        Assert.assertTrue(result4);
+        Assert.assertTrue(result5);
+    }
+
+//    @Test
+//    public void addOverlapping() {
+//        claims[0].addOverlapping(claims[1]);
+//
+//        actualResult
+//    }
 }
