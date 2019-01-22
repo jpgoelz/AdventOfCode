80096b9ea661d8ae72f7c3efaba7351e0e13fc72
Jan Philipp Gölz <basseur@mail.de>
2019-01-22 22:18:50 +0100

    AOC-011: New Claim class and ClaimTest

--- src/main/java/org/basseur/adventofcode/advent2018/utils/Claim.java
+++ src/main/java/org/basseur/adventofcode/advent2018/utils/Claim.java
@@ -1,73 +1,40 @@
 package org.basseur.adventofcode.advent2018.utils;
 
+import java.awt.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Claim {
-    //    HashMap<Integer, Claim> overlappingClaims;
     private int id;
-    private int x1;
-    private int y1;
-    private int width;
-    private int height;
-
-    private int x2;
-    private int y2;
+    private Rectangle rectangle;
 
     public Claim(String claimsListItem) {
         Pattern pattern = Pattern.compile("#(\\d+) @ (\\d+),(\\d+): (\\d+)x(\\d+)");
         Matcher matcher = pattern.matcher(claimsListItem);
         while (matcher.find()) {
             this.id = Integer.parseInt(matcher.group(1));
-            this.x1 = Integer.parseInt(matcher.group(2));
-            this.y1 = Integer.parseInt(matcher.group(3));
-            this.width = Integer.parseInt(matcher.group(4));
-            this.height = Integer.parseInt(matcher.group(5));
+            int x = Integer.parseInt(matcher.group(2));
+            int y = Integer.parseInt(matcher.group(3));
+            int width = Integer.parseInt(matcher.group(4));
+            int height = Integer.parseInt(matcher.group(5));
+            this.rectangle = new Rectangle(x, y, width, height);
         }
-        x2 = x1 + width;
-        y2 = y1 + height;
     }
 
-    int getId() {
+    public int getId() {
         return id;
     }
 
-    int getX1() {
-        return x1;
+    public Rectangle getRectangle() {
+        return rectangle;
     }
 
-    int getY1() {
-        return y1;
-    }
-
-    int getWidth() {
-        return width;
-    }
-
-    int getHeight() {
-        return height;
-    }
-
-    public int getX2() {
-        return x2;
-    }
-
-    public int getY2() {
-        return y2;
-    }
-
-    public void addOverlapping(Claim claim) {
+    public Rectangle overlap(Claim other) {
+        return this.rectangle.intersection(other.rectangle);
     }
 
     boolean overlaps(Claim other) {
-        if (this.x2 < other.x1 || this.x1 > other.x2) {
-            return false;
-        }
-        if (this.y2 < other.y1 || this.y1 > other.y2) {
-            return false;
-        } else {
-            return true;
-        }
+        return this.rectangle.intersects(other.rectangle);
     }
 //    HashMap
 }
--- src/test/java/org/basseur/adventofcode/advent2018/utils/ClaimTest.java
+++ src/test/java/org/basseur/adventofcode/advent2018/utils/ClaimTest.java
@@ -5,16 +5,19 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.awt.*;
+
 @RunWith(SpringRunner.class)
 public class ClaimTest {
 
     @Test
     public void Claim() {
         Claim claim = new Claim("#1 @ 2,3: 4x5");
-        int[] expectedResult = new int[]{1, 2, 3, 4, 5};
-        int[] actualResult = new int[]{claim.getId(), claim.getX1(), claim.getY1(), claim.getWidth(), claim.getHeight()};
+        int expectedId = 1;
+        Rectangle expectedRectangle = new Rectangle(2, 3, 4, 5);
 
-        Assert.assertArrayEquals(expectedResult, actualResult);
+        Assert.assertEquals(expectedId, claim.getId());
+        Assert.assertEquals(expectedRectangle, claim.getRectangle());
     }
 
     /**
@@ -23,7 +26,6 @@ public class ClaimTest {
      * claim4 overlaps claim1 on the lower right corner.
      * claim4 overlaps claim2 on the upper right corner.
      * claim5 completely surrounds claim1 (overlaps).
-     * Gute Ideen zum testen: https://www.baeldung.com/java-check-if-two-rectangles-overlap
      */
     @Test
     public void overlaps() {
@@ -46,10 +48,30 @@ public class ClaimTest {
         Assert.assertTrue(result5);
     }
 
-//    @Test
-//    public void addOverlapping() {
-//        claims[0].addOverlapping(claims[1]);
-//
-//        actualResult
-//    }
+    @Test
+    public void overlap() {
+        Claim claim1 = new Claim("#1 @ 2,2: 4x3");
+        Claim claim2 = new Claim("#2 @ 2,6: 4x3");
+        Claim claim3 = new Claim("#3 @ 7,2: 4x3");
+        Claim claim4 = new Claim("#4 @ 4,4: 4x3");
+        Claim claim5 = new Claim("#5 @ 1,1: 6x5");
+
+        Rectangle expectedOverlap1 = new Rectangle(2, 6, 4, -1);
+        Rectangle expectedOverlap2 = new Rectangle(7, 2, -1, 3);
+        Rectangle expectedOverlap3 = new Rectangle(4, 4, 2, 1);
+        Rectangle expectedOverlap4 = new Rectangle(4, 6, 2, 1);
+        Rectangle expectedOverlap5 = new Rectangle(2, 2, 4, 3);
+
+        Rectangle actualOverlap1 = claim1.overlap(claim2);
+        Rectangle actualOverlap2 = claim1.overlap(claim3);
+        Rectangle actualOverlap3 = claim1.overlap(claim4);
+        Rectangle actualOverlap4 = claim2.overlap(claim4);
+        Rectangle actualOverlap5 = claim1.overlap(claim5);
+
+        Assert.assertEquals(expectedOverlap1, actualOverlap1);
+        Assert.assertEquals(expectedOverlap2, actualOverlap2);
+        Assert.assertEquals(expectedOverlap3, actualOverlap3);
+        Assert.assertEquals(expectedOverlap4, actualOverlap4);
+        Assert.assertEquals(expectedOverlap5, actualOverlap5);
+    }
 }
