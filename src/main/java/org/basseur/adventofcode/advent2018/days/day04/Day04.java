a5cc180607eea705d5048131785a44a6aa60871c
Jan Philipp Gölz <basseur@mail.de>
2019-01-27 02:27:16 +0100

    AOC-021: Updated Structure

--- src/main/java/org/basseur/adventofcode/advent2018/days/day04/Day04.java
+++ src/main/java/org/basseur/adventofcode/advent2018/days/day04/Day04.java
@@ -1,7 +1,9 @@
-package org.basseur.adventofcode.advent2018.days;
+package org.basseur.adventofcode.advent2018.days.day04;
 
 import org.basseur.adventofcode.advent2018.ProblemStatusEnum;
+import org.basseur.adventofcode.advent2018.days.Days;
 import org.basseur.adventofcode.advent2018.utils.FileReaders;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
@@ -10,17 +12,18 @@ import java.util.List;
 @Component
 public class Day04 implements Days {
 
-    private static final String FILE_LOCATION = "src/main/java/org/basseur/adventofcode/advent2018/days/Day04Input.txt";
-    private List<String> guardRecords;
+    private static final String FILE_LOCATION = "src/main/java/org/basseur/adventofcode/advent2018/days/Input.txt";
+    private final List<String> guardRecords;
 
-    private HashMap<String, ProblemStatusEnum> problemStatus;
+    private final HashMap<String, ProblemStatusEnum> problemStatus;
 
-    public Day04() {
+    @Autowired
+    Day04(FileReaders fileReaders) {
         this.problemStatus = new HashMap<>();
         this.problemStatus.put("1", ProblemStatusEnum.IN_PROGRESS);
         this.problemStatus.put("2", ProblemStatusEnum.UNSOLVED);
 
-        this.guardRecords = FileReaders.readFileIntoStringList(FILE_LOCATION);
+        this.guardRecords = fileReaders.readFileIntoStringList(FILE_LOCATION);
     }
 
     @Override
@@ -49,7 +52,7 @@ public class Day04 implements Days {
      *
      * @return product of the <u>ID</u> of the guard who slept the longest and the <u>minute</u> he slept the most.
      */
-    private String getIdMultipliedByMinute() {
-        return null;
+    private int getIdMultipliedByMinute() {
+        return 0;
     }
 }
---
+++ src/test/java/org/basseur/adventofcode/advent2018/days/day04/GuardTest.java
@@ -0,0 +1,16 @@
+package org.basseur.adventofcode.advent2018.days.day04;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class GuardTest {
+
+    @Ignore
+    @Test
+    public void notATest() {
+
+    }
+}
