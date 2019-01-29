39e9a3f406363657fd0558341e94710bf8ab3fe5
Jan Philipp Gölz <basseur@mail.de>
2019-01-29 20:55:08 +0100

    AOC-021: Finished `Guard` class

--- src/main/java/org/basseur/adventofcode/advent2018/days/day04/Guard.java
+++ src/main/java/org/basseur/adventofcode/advent2018/days/day04/Guard.java
@@ -1,5 +1,7 @@
 package org.basseur.adventofcode.advent2018.days.day04;
 
+import java.util.stream.IntStream;
+
 /**
  * A class that defines Guards to be used in {@link Day04}.
  *
@@ -8,6 +10,7 @@ package org.basseur.adventofcode.advent2018.days.day04;
 class Guard {
     /** ID of this guard */
     private final int id;
+    private final int[] sleepPerMinute = new int[60];
 
     /**
      * A constructor setting the ID
@@ -25,5 +28,42 @@ class Guard {
      */
     int getID() {
         return this.id;
+    }
+
+    /**
+     * Adds sleep to th internal {@code sleepPerMinute} array.
+     * Each element of the array in the argument  is added to each element in the internal array.
+     *
+     * @param minutesSlept {@code int[]} of minutes slept
+     */
+    void addSleep(int[] minutesSlept) {
+        IntStream.range(0, 60).forEach(minute -> this.sleepPerMinute[minute] += minutesSlept[minute]);
+    }
+
+    /**
+     * Returns the total amount of sleep in minutes
+     *
+     * @return total amount of sleep in minutes
+     */
+    int getTotalSleep() {
+        return IntStream.of(sleepPerMinute).sum();
+    }
+
+    /**
+     * Returns the minute of maximum sleep
+     *
+     * @return minute of maximum sleep
+     */
+    int getMinuteOfMaximumSleep() {
+        int minuteOfMaximumSleep = 0;
+        int currentMaxSleepPerMinute = 0;
+
+        for (int minute = 0; minute < 60; minute++) {
+            if (sleepPerMinute[minute] > currentMaxSleepPerMinute) {
+                minuteOfMaximumSleep = minute;
+                currentMaxSleepPerMinute = sleepPerMinute[minute];
+            }
+        }
+        return minuteOfMaximumSleep;
     }
 }
--- src/test/java/org/basseur/adventofcode/advent2018/days/day04/GuardTest.java
+++ src/test/java/org/basseur/adventofcode/advent2018/days/day04/GuardTest.java
@@ -1,16 +1,42 @@
 package org.basseur.adventofcode.advent2018.days.day04;
 
-import org.junit.Ignore;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.stream.IntStream;
+
 @RunWith(SpringRunner.class)
 public class GuardTest {
 
-    @Ignore
     @Test
-    public void notATest() {
+    public void testGetMinuteOfMaximumSleep() {
+        int[] minutesOfSleep = new int[60];
+        minutesOfSleep[15] = 13;
+        minutesOfSleep[30] = 42;
+        minutesOfSleep[45] = 1337;
 
+        Guard guard = new Guard(1);
+        guard.addSleep(minutesOfSleep);
+
+        int expectedResult = 45;
+        int actualResult = guard.getMinuteOfMaximumSleep();
+
+        Assert.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testGetTotalSleep() {
+        int[] minutesOfSleep = new int[60];
+        IntStream.range(0, 30).forEach(minute -> minutesOfSleep[minute] = 1);
+
+        Guard guard = new Guard(1);
+        guard.addSleep(minutesOfSleep);
+
+        int expectedResult = 30;
+        int actualResult = guard.getTotalSleep();
+
+        Assert.assertEquals(expectedResult, actualResult);
     }
 }
