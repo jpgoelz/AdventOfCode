22f4c09847b972902a7322b0ff9190f381673446
Jan Philipp Gölz <basseur@mail.de>
2019-01-29 18:16:31 +0100

    AOC-021: WIP

--- src/main/java/org/basseur/adventofcode/advent2018/days/day04/Day04.java
+++ src/main/java/org/basseur/adventofcode/advent2018/days/day04/Day04.java
@@ -6,14 +6,19 @@ import org.basseur.adventofcode.advent2018.utils.FileReaders;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.text.Collator;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Component
 public class Day04 implements Days {
 
     private static final String FILE_LOCATION = "src/main/java/org/basseur/adventofcode/advent2018/days/day04/Input.txt";
     private final List<String> guardRecords;
+    private final Map<Integer, Guard> guardHashMap = new HashMap<>();
 
     private final HashMap<String, ProblemStatusEnum> problemStatus;
 
@@ -24,6 +29,7 @@ public class Day04 implements Days {
         this.problemStatus.put("2", ProblemStatusEnum.UNSOLVED);
 
         this.guardRecords = fileReaders.readFileIntoStringList(FILE_LOCATION);
+        this.guardRecords.sort(Collator.getInstance());
     }
 
     @Override
@@ -43,7 +49,23 @@ public class Day04 implements Days {
 
     @Override
     public HashMap<String, ProblemStatusEnum> getProblemStatus() {
-        return null;
+        return problemStatus;
+    }
+
+
+    /**
+     * Parses all the Guards from the {@code guardRecords} and adds them to {@code HashMap<Integer, Guard> guardHashMap}.
+     */
+    private void parseGuards() {
+        for (String guardRecord : guardRecords) {
+            Pattern pattern = Pattern.compile("\\[\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}\\] Guard #(\\d+) begins shift");
+            Matcher matcher = pattern.matcher(guardRecord);
+            int id = 0;
+            if (matcher.find()) {
+                id = Integer.parseInt(matcher.group(1));
+                guardHashMap.put(id, new Guard(id));
+            }
+        }
     }
 
     /**
@@ -53,6 +75,7 @@ public class Day04 implements Days {
      * @return product of the <u>ID</u> of the guard who slept the longest and the <u>minute</u> he slept the most.
      */
     private int getIdMultipliedByMinute() {
+        parseGuards();
         return 0;
     }
 }
