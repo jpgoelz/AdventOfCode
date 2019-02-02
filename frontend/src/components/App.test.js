791c5244effe8a9ac21c55a45e1474a4488bf709
mfbieber <mfbieber@gmail.com>
2019-02-02 02:25:48 +0100

    added testing for dynamic adding of cards and in the correct order

--- frontend/src/components/App.test.js
+++ frontend/src/components/App.test.js
@@ -4,12 +4,16 @@ import App from "./App";
 import { shallow } from "enzyme";
 
 describe("App", () => {
-  it("should add puzzle cards", () => {
+  it("should add puzzle cards provided by the state", () => {
     const component = shallow(<App />).dive();
-    const expectedResults = [
-      "Part 1 - Frequency: 599",
-      "Part 2 - Frequency reached twice: 81204"
-    ];
+    component.setState({
+      solved: {
+        1: { day: "1", part: "1", result: "abc" },
+        2: { day: "1", part: "2", result: "def" },
+        3: { day: "3", part: "1", result: "ghi" }
+      }
+    });
+    const expectedResults = ["abc", "def", "ghi"];
     const actualPuzzleCards = component.instance().addPuzzleCards();
 
     expect(
@@ -18,14 +22,21 @@ describe("App", () => {
 
     expect(
       actualPuzzleCards.map(card => card.props.cardType)
-    ).to.have.all.members(["puzzleCard", "puzzleCard"]);
+    ).to.have.all.members(["puzzleCard", "puzzleCard", "puzzleCard"]);
 
     expect(actualPuzzleCards.map(card => card.props.day)).to.have.all.members([
       "1",
-      "1"
+      "1",
+      "3"
     ]);
 
-    expect(actualPuzzleCards.length).to.equal(2);
+    expect(actualPuzzleCards.length).to.equal(3);
+
+    let cardResults = [];
+    actualPuzzleCards.forEach(card => cardResults.push(card.props.result));
+    expect(cardResults[0]).to.equal("ghi");
+    expect(cardResults[1]).to.equal("def");
+    expect(cardResults[2]).to.equal("abc");
   });
 
   it("should render the desired childComponents", () => {
