5f063bcd9a9032a5340ee2ddc8571c3140dba0c9
Jan Philipp Gölz <basseur@mail.de>
2019-05-25 01:08:33 +0200

    AOC-025: Corrected tests

--- src/test/java/org/basseur/adventofcode/advent2018/days/day08/Day08Test.java
+++ src/test/java/org/basseur/adventofcode/advent2018/days/day08/Day08Test.java
@@ -11,21 +11,17 @@ import org.mockito.Mockito;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @RunWith(SpringRunner.class)
 public class Day08Test {
 
     @MockBean
     private FileReaders fileReaders;
     private Days day08;
-    private List<String> nodeString = new ArrayList<>();
+    private Integer[] treeData = {2, 3, 0, 3, 10, 11, 12, 1, 1, 0, 1, 99, 2, 1, 1, 2};
 
     @Before
     public void setUp() {
-        nodeString.add("2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2");
-        Mockito.when(fileReaders.readFileIntoStringList(Mockito.anyString())).thenReturn(nodeString);
+        Mockito.when(fileReaders.readFileIntoArrayOfIntegers(Mockito.anyString())).thenReturn(treeData);
         day08 = new Day08(fileReaders);
     }
 
