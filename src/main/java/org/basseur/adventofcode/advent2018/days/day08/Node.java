5a24a4f87a4495f32aed08c8b448a5ee18e20d8d
Jan Philipp Gölz <basseur@mail.de>
2019-05-25 01:55:25 +0200

    AOC-025: replaced sub with child as in the puzzle description

--- src/main/java/org/basseur/adventofcode/advent2018/days/day08/Node.java
+++ src/main/java/org/basseur/adventofcode/advent2018/days/day08/Node.java
@@ -3,12 +3,12 @@ package org.basseur.adventofcode.advent2018.days.day08;
 import java.util.Arrays;
 
 public class Node {
-    /** The length of the header the containing amount of sub nodes and the length of metadata */
+    /** The length of the header the containing amount of child nodes and the length of metadata */
     private static final int headerLength = 2;
     /** The metadata of this node */
     private int[] metadata;
-    /** The sub nodes of this node */
-    private Node[] subNodes;
+    /** The child nodes of this node */
+    private Node[] childNodes;
 
     /**
      * The constructor parses the nodes array and instantiates its own child nodes, while also storing the meta data
@@ -16,16 +16,16 @@ public class Node {
      * @param nodes the array of nodes to be parsed
      */
     Node(Integer[] nodes) {
-        int amountOfSubNodes = nodes[0];
+        int numberOfChildNodes = nodes[0];
         int metadataLength = nodes[1];
 
-        subNodes = new Node[amountOfSubNodes];
-        int currentSubNodeStart = headerLength;
+        childNodes = new Node[numberOfChildNodes];
+        int currentChildNodeStart = headerLength;
 
-        for (int i = 0; i < amountOfSubNodes; i++) {
-            subNodes[i] = new Node(Arrays.copyOfRange(nodes, currentSubNodeStart, nodes.length));
+        for (int i = 0; i < numberOfChildNodes; i++) {
+            childNodes[i] = new Node(Arrays.copyOfRange(nodes, currentChildNodeStart, nodes.length));
 
-            currentSubNodeStart += subNodes[i].getLength();
+            currentChildNodeStart += childNodes[i].getLength();
         }
 
         metadata = new int[metadataLength];
@@ -42,8 +42,8 @@ public class Node {
      */
     private int getLength() {
         int length = headerLength + metadata.length;
-        for (Node subNode : subNodes) {
-            length += subNode.getLength();
+        for (Node childNode : childNodes) {
+            length += childNode.getLength();
         }
         return length;
     }
@@ -52,7 +52,7 @@ public class Node {
      * Primary method for Day 8, Part 1.
      * <p>
      * Calculates the sum of all metadata entries. For this purpose it first calculates the sum of the
-     * metadata of this node and then calls this method for each of the sub nodes recursively. The values
+     * metadata of this node and then calls this method for each of the child nodes recursively. The values
      * are added up and returned.
      *
      * @return the sum of all metadata of this and the child nodes
@@ -62,8 +62,8 @@ public class Node {
         for (int metadatum : metadata) {
             sum += metadatum;
         }
-        for (Node subNode : subNodes) {
-            sum += subNode.getSumOfMetadata();
+        for (Node childNode : childNodes) {
+            sum += childNode.getSumOfMetadata();
         }
         return sum;
     }
@@ -71,24 +71,24 @@ public class Node {
     /**
      * Primary method for Day 8, Part 2.
      * <p>
-     * Determines the value of this node. If it has no sub nodes, the value is the sum of this nodes metadata.
+     * Determines the value of this node. If it has no child nodes, the value is the sum of this nodes metadata.
      * If it has children, the value is calculated the following way:
-     * Each metadata entry represents a reference to the sub nodes of this node (e.g. 1 is the first sub node).
-     * The value of the referenced sub node is obtained by calling this method of that sub node. This way, all
-     * the values of the sub nodes are added up recursively and returned.
+     * Each metadata entry represents a reference to the child nodes of this node (e.g. 1 is the first child node).
+     * The value of the referenced child node is obtained by calling this method of that child node. This way, all
+     * the values of the child nodes are added up recursively and returned.
      *
      * @return the value of this node
      */
     public int getNodeValue() {
         int value = 0;
-        if (subNodes.length == 0) {
+        if (childNodes.length == 0) {
             for (int metadatum : metadata) {
                 value += metadatum;
             }
         } else {
             for (int metadatum : metadata) {
-                if (metadatum <= subNodes.length) {
-                    value += subNodes[metadatum - 1].getNodeValue();
+                if (metadatum <= childNodes.length) {
+                    value += childNodes[metadatum - 1].getNodeValue();
                 }
             }
         }
